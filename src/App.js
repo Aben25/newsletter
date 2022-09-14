fc9f3063368bdbe128d85b4c13702c6f4df6e79a
Abenezer Nuro <abenuro@gmail.com>
2022-09-14 00:25:16 -0400

    done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import { createContext, useState } from 'react';
 import './App.css';
 import Form from './components/Form';
 
+export const UserContext = createContext();
 function App() {
-  return <div className="App">
-    <Form/>
-  </div>;
+const [data,setData]=useState({ name: '', email: '' });
+
+  return (
+    <div className="App">
+      <UserContext.Provider value={{data,setData}}>
+      <Form />
+      </UserContext.Provider>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -7,38 +7,50 @@ import {
   AppBar,
   TextField,
   Button,
+  toolbar,
 } from "@mui/material";
-class Form extends Component {
-    render() {
-        return (
-          <div>
-            <AppBar position="static">
-              <toolbar>
-                <h1>SIGNIN FORM </h1>
-              </toolbar>
-            </AppBar>
-            <FormControl>
-              <TextField
-                style={{ width: "200px", margin: "5px" }}
-                type="text"
-                label="Name"
-                variant="outlined"
-              />
-              <br />
-              <TextField
-                style={{ width: "200px", margin: "5px" }}
-                type="text"
-                label="Email"
-                variant="outlined"
-              />
-              <br />
-              <Button variant="contained" color="primary">
-                Submit
-              </Button>
-            </FormControl>
-          </div>
-        );
-    }
-}
+import { useContext, useState } from "react";
+import { UserContext } from '../App';
 
-export default Form;
+export default function Form() {
+
+    const {   setData, data } = useContext(UserContext);
+
+    const chandler = (e) => {
+      console.log('====================================');
+      console.log(data);
+      console.log('====================================');
+e.preventDefault();    };
+
+  return (
+    <div>
+      <div>
+        <AppBar position="static">
+          <h1>SIGNIN FORM </h1>
+        </AppBar>
+        <FormControl>
+          <TextField
+            style={{ width: "200px", margin: "5px" }}
+            type="text"
+            label="Name"
+            variant="outlined"
+            onChange={(e) => setData({ name: e.target.value })}
+          />
+          <br />
+          <TextField
+            style={{ width: "200px", margin: "5px" }}
+            type="text"
+            label="Email"
+            variant="outlined"
+            onChange={(e) => setData({ email: e.target.value })}
+          />
+          <br />
+          <Button variant="contained" color="primary"
+          onClick={() => chandler(event)}>
+            Submit
+          </Button>
+        </FormControl>
+      </div>
+    </div>
+  );
+}
